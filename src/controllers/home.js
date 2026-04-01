62616ed536eddcaa90cd1662c50a0a95582bd4aa
Larson04 <lar23016@byui.edu>
2026-04-01 14:05:57 -0600

    Bro Keers looking at it

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -3,6 +3,7 @@ import { getFeaturedVehicles } from '../models/inventory/vehicles.js';
 export const homePage = async (req, res) => {
   const featuredVehicles = await getFeaturedVehicles(5, 'year');
 
+
   res.render('home', {
     title: 'Welcome to Our Dealership',
     featuredVehicles
